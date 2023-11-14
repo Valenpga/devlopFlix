2dce4ff81d6bbaf4c8abcfec1847b1b144e4185e
Valenpga <131762425+Valenpga@users.noreply.github.com>
2023-11-14 20:54:14 +0100

    Update script.js

--- developflix/js/script.js
+++ developflix/js/script.js
@@ -1,5 +1,41 @@
 import peliculas from '/peliculas.js'
 
+//genre_ids: genero
+//poster_path: imagenes
+//title: titulo
+
+
+const mostrarPeliculaPorGenero = (generoId,contenedorId) => {
+    const contenedor = document.getElementById(contenedorId)
+    const peliculaGenero = peliculas.filter((pelicula) => pelicula.genre_includes(generoId))
+    peliculaGenero.forEach => {   
+        let templatePelis = ``
+        <div class="pelicula">
+            <img src="https//image.tmdb.org/t/p/w200/${pelicula.poster_path}" alt="${pelicula.title}">
+            <h3>${pelicula.title}</h3>
+        <div/>
+    contenedor.innerHTML += templatePelis
+    })
+    
+}
+
+
+
+mostrarPeliculaPorGenero(28, "genero-28")
+mostrarPeliculaPorGenero(53, "genero-53")
+mostrarPeliculaPorGenero(12, "genero-12")
+
+
+
+
+
+
+
+
+
+
+
+
 /*const accion = document.getElementById("genero-28");
 const thriller = document.getElementById("genero-53");
 const aventura = document.getElementById("genero-12");
