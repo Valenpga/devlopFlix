102efaa1fd23a261b2ed65c15a7369872769ebc2
Valenpga <131762425+Valenpga@users.noreply.github.com>
2023-11-14 19:34:01 +0100

    Add files via upload

---
+++ developflix/js/script.js
@@ -0,0 +1,25 @@
+import peliculas from '/peliculas.js'
+
+/*const accion = document.getElementById("genero-28");
+const thriller = document.getElementById("genero-53");
+const aventura = document.getElementById("genero-12");
+
+document.getElementById("genero-28").innerHTML = accion.filter(checkGenero);
+function checkGenero(peliculas){
+    return peliculas = genero-28
+    console.log(peliculas)
+}
+
+
+/*const cartelera = ['Muzzle','Retribution','Fax X','the Equalizer','Gran Turismo','Expend4bles','Mission impossible',
+'Sound of Freedom','Desperation Road','Uri','Boudica','Blue Beetle', 'Paw Patrol','Sniper G.R.I.T.',
+'Transformers','The Sworsman','Spider Man','Babylon A.D','The Enginer'];
+connsole.log(cartelera);*/
+
+
+
+
+
+
+
+
